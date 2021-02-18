612092d3334208f8882e7e44983c075b66f36bb7
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-02-18 23:26:56 +0300

    preventing the browser from realoding

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 
 function App() {
   const [input, setinput] = useState ("");
+  const [messages, setMessages] =useState ([]);
   console.log (input);
-  
+  console.log(messages);
+
+  const sendMessage = (event) =>{
+    // All the logic to send the message goes here 
+    event.preventDefault();
+    setMessages ([...messages, input]);
+    setinput('');
+  }
   return (
     <div className="App">
       <h1>Hello There am clean</h1>
+
+      <form>
       <input value = {input} onChange = {event => setinput(event.target.value)}/>
-      <button>Send Message </button>
+      <button type = "submit" onClick = {sendMessage}>Send Message </button>
+      </form>
+{
+      messages.map(message => (
+        <p>{message}</p>
+      ))
+}
     </div>
   );
 }
