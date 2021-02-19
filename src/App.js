4f0665cb603108990d0943405706b0588a3c3d7f
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-02-19 10:30:16 +0300

    Styling the messages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React,{ useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
 
 function App() {
   const [input, setinput] = useState ("");
-  const [messages, setMessages] =useState ([]);
-  console.log (input);
-  console.log(messages);
+  const [messages, setMessages] =useState ([
+    {username: 'Mark', text: 'I am happy with react'},
+    {username: 'Shammah', text: 'I am happy comming'}
+  ]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    //prompt ('Please enter your name... ')
+    setUsername(prompt('Please Enter your name'))
+  }, [])
 
   const sendMessage = (event) =>{
     // All the logic to send the message goes here 
     event.preventDefault();
-    setMessages ([...messages, input]);
+    setMessages ([
+      ...messages, {username:username, text: input}]);
     setinput('');
   }
   return (
     <div className="App">
       <h1>Hello There am clean</h1>
-
+      <h2>Welcome {username}</h2>
       <form>
   <FormControl>
   <InputLabel>Enter a message..</InputLabel>
@@ -29,7 +37,7 @@ function App() {
       </form>
 {
       messages.map(message => (
-        <Message text = {message}/>
+        <Message username = {username} message = {message}/>
       ))
 }
     </div>
